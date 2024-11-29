8736edd9b99bbeb8937914f3fa81d4d3a3ee0b87
akashkumar8115 <akash2884182@gmail.com>
2024-11-29 15:28:35 +0530

    index.js updated

--- src/index.js
+++ src/index.js
@@ -10,11 +10,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
   <BrowserRouter>
-      <App />
+    <App />
   </BrowserRouter>
 
 
- 
+
 );
 
 // If you want to start measuring performance in your app, pass a function
